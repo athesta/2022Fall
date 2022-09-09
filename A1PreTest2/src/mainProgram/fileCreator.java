18ff4e5df10fed69b422aa9ca9a8fe986b7ecac4
Jen Swofford <jen.l.swofford@gmailcom>
2022-09-08 22:05:11 -0500

    update

--- A1PreTest2/src/mainProgram/fileCreator.java
+++ A1PreTest2/src/mainProgram/fileCreator.java
@@ -9,8 +9,10 @@ import splittingstrings.*;
 public class fileCreator {
 
 	public static void main(String[] args)throws java.io.FileNotFoundException, java.io.IOException {
+		String input = null;
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(new FileReader(input));
+		Writer ouput = null;
 		PrintWriter outFile = new PrintWriter(ouput);
 		
 		
