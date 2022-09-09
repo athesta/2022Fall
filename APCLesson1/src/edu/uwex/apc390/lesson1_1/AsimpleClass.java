35cb19226ac8fb72adc9cc685f6f275e80e61502
athesta <89466011+athesta@users.noreply.github.com>
2022-09-08 21:25:15 -0500

    Delete APCLesson1 directory

--- APCLesson1/src/edu/uwex/apc390/lesson1_1/AsimpleClass.java
+++
@@ -1,7 +0,0 @@
-package edu.uwex.apc390.lesson1_1;
-
-public class AsimpleClass {
-	public static void main(String[] args) {
-		System.out.println("Welcome to APC 390 Object Oriented Programming!");
-	}
-}
