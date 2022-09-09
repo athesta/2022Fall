fade73d44fa0809ce9ec8519430004e39b99b015
Jen Swofford <jen.l.swofford@gmailcom>
2022-09-08 22:45:25 -0500

    k

--- A1PreTest2/src/mainProgram/fileCreator.java
+++ A1PreTest2/src/mainProgram/fileCreator.java
@@ -1,3 +1,14 @@
+/*
+ * Purpose
+You will be building code collaboratively to practice how to use string methods. By working collaboratively, you can help each other figure out how to write programs correctly. To prepare for this discussion, it might help to review Chapter 4, (pp. 223–230).
+
+Instructions
+As a group, we will write a program to correctly read in a file containing a list of songs and write it out to another file.
+
+A “songs.txt”  Download “songs.txt”file of the following form is supplied.
+We will do this one line of code at a time.
+ */
+
 package mainProgram;
 
 import java.io.*;
