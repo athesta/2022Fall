75d6d92d77003a3151af13db982bb0db8088f075
Jen Swofford <jen.l.swofford@gmailcom>
2022-09-08 21:44:14 -0500

    update

--- A1PreTest2/src/mainProgram/fileCreator.java
+++ A1PreTest2/src/mainProgram/fileCreator.java
@@ -1,6 +1,8 @@
 package mainProgram;
 
 import java.io.*;
+import java.util.Scanner;
+
 import files.*;
 import splittingstrings.*;
 
@@ -8,6 +10,10 @@ public class fileCreator {
 
 	public static void main(String[] args)throws java.io.FileNotFoundException, java.io.IOException {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(new FileReader(input));
+		PrintWriter outFile = new PrintWriter(ouput);
+		
+		
 
 
 	}
