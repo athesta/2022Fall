8642a66f95253a1c022d80dd47c4b96fe91539fa
Jen Swofford <jen.l.swofford@gmailcom>
2022-09-16 20:23:30 -0500

    So many Updates

--- AssignmentOne/src/tables/TableRow.java
+++ AssignmentOne/src/tables/TableRow.java
@@ -1,9 +1,33 @@
 package tables;
 
+import java.util.*;
+
 public class TableRow {
 	
+
+
 	//Data Members
 	private String[] rowData = new String [3];
+	
+	
 
 
+	//Constructor
+	public TableRow(String number, String name, String value) {
+		rowData[0] = number;
+		rowData[1] = name;
+		rowData[2] = value;
+	}
+		
+
+
+	public String toString() {
+		 
+		 return String.join(" ", rowData); 
+		 
+	}
+	
+
+	
+	
 }
--- AssignmentOne/src/tables/TextTable.java
+++ AssignmentOne/src/tables/TextTable.java
@@ -9,7 +9,7 @@ public class TextTable {
 	private String headerInfo;
 	// A private data member: an array of 100 TableRow objects.
 	private TableRow[] fullTable = new TableRow[100];
-	private int numRows = 0;
+	private int numRows=0;
 
 
 	//loads the table file given the file name as a String
@@ -19,6 +19,7 @@ public class TextTable {
 			Scanner inFile = new Scanner (new FileReader(fileName));
 			
 				headerInfo = inFile.nextLine();
+				
 				while (inFile.hasNextLine()) {
 					String Line = inFile.nextLine();
 					String split[] = Line.split("\\s+");
@@ -26,16 +27,16 @@ public class TextTable {
 					String name = split[1];
 					String value = split[2];
 					addRow(number, name, value);
-				}
+						
+				//System.out.println("TEST HEADER: " + headerInfo + " TEST CURRENT ROW #: " + numRows);
+				//System.out.println(Arrays.deepToString(fullTable));
+				}	
+				inFile.close();
 				
-				System.out.println(headerInfo + " " + numRows);
-				
-			
-			
-			
-			
-			inFile.close();
-		} catch (java.io.FileNotFoundException fnfex) {
+		}
+		 
+		
+		catch (java.io.FileNotFoundException fnfex) {
 			System.err.println("Your file cannot be located. Please check your file name or file location.");
 		}
 	}
@@ -44,19 +45,31 @@ public class TextTable {
 	
 	public void saveTableToFile(String fileName) throws FileNotFoundException {
 		PrintWriter outFile = new PrintWriter(fileName);
+		
+		
+		outFile.println(headerInfo + "\n");
+		for (int i = numRows-numRows; i < fullTable.length && fullTable[i] != null; i++) {
+				outFile.println(fullTable[i]);
+							
+		}
 		outFile.flush();
 		outFile.close();
 	}
 
 	public void addRow(String number, String name, String value) {
-		TableRow[] singleRow = new TableRow[3];
-		//still need to figure this out....
-		singleRow[0] = number;
-		singleRow[1] = name;
-		singleRow[2] = value;
 		
+		TableRow singleRow = new TableRow(number, name, value);
+		fullTable[numRows] = singleRow;
+		/*
+		 * SOME QA STUFF: 
+		 * System.out.println (" TEST: Array for Row " + numRows +": \n");
+		 * System.out.println(singleRow.toString());
+		 * System.out.println(fullTable.toString()); System.out.println("\n ENDTEST");
+		 */
+		numRows++;
 	}
-
+	
+	
 	public void removeRow(String number) {
 		// TODO Auto-generated method stub
 		
